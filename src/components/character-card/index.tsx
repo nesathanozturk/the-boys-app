6d635ec8604d6f39ffe1fd7e919dd523e80c4a2c
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-06-29 00:24:41 +0300

    refactor: edited card ddetail

--- src/components/character-card/index.tsx
+++ src/components/character-card/index.tsx
@@ -5,26 +5,28 @@ import { ICharacterProps } from "@/types";
 
 const CharacterCard: React.FC<ICharacterProps> = ({ character }) => {
   return (
-    <div key={character?.id} className={styles.card}>
-      <div className={styles.box}>
-        <Image
-          src={character?.image}
-          alt={character?.name}
-          className={styles.image}
-          width={100}
-          height={100}
-        />
-        <div className={styles.overlay}>
-          <Link href={`character/${character?.id}`} className={styles.link}>
-            See details
-          </Link>
+    <>
+      <div className={styles.card}>
+        <div className={styles.box}>
+          <Image
+            src={character?.image}
+            alt={character?.name}
+            className={styles.image}
+            width={100}
+            height={100}
+          />
+          <div className={styles.overlay}>
+            <Link href={`character/${character?.id}`} className={styles.link}>
+              See details
+            </Link>
+          </div>
+        </div>
+        <div className={styles.body}>
+          <h3 className={styles.title}>{character?.name}</h3>
+          <p className={styles.altTitle}>{character?.portrayed[0]}</p>
         </div>
       </div>
-      <div className={styles.body}>
-        <h3 className={styles.title}>{character?.name}</h3>
-        <p className={styles.altTitle}>{character?.portrayed[0]}</p>
-      </div>
-    </div>
+    </>
   );
 };
 
