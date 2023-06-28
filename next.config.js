dfef4b6ad012e579a04c17d6aec18a043eb4480b
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-06-28 20:01:12 +0300

    feat: added domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "theboysapi.s3.us-west-2.amazonaws.com",
+      },
+      {
+        hostname: "www.theboysapi.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
