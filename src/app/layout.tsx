579b0f3210086c042decc0b2b84a8db19537a1a4
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-06-28 19:15:07 +0300

    feat: imported global css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Poppins } from "next/font/google";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
-import "./globals.css";
+import "../styles/globals.css";
 
 const poppins = Poppins({
   weight: "400",
